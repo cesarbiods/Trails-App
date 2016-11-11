ca4f8c9f019fe5135bc627bef5ad43218061fa30
Apebble38 <clawrenc@oswego.edu>
2016-11-11 15:17:36 -0500

    updated trail and trailList class

--- app/src/main/java/com/example/mikey/maps/Trail.java
+++ app/src/main/java/com/example/mikey/maps/Trail.java
@@ -1,5 +1,7 @@
 package com.example.mikey.maps;
 
+import android.location.Location;
+
 /**
  * Created by Workstation2.0 on 11/9/2016.
  */
@@ -12,12 +14,9 @@ public class Trail {
     int rating;
     String type;
 
-    public Trail(String name, double latitude, double longtitude, String type){
+    public Trail(String name, String latitude, String longtitude, String type){
         this.name = name;
-        this.latitude = latitude;
-        this.longtitude = longtitude;
-        location.setLatitude(latitude);
-        location.setLongitude(longtitude);
+        setLocation(latitude,longtitude);
         this.type = type;
     }
 
@@ -25,6 +24,38 @@ public class Trail {
         return this.name;
     }
 
+    private void setLocation(String latitude, String longtitude){
+        String orentation;
+        //latitude parsing
+        String[] latTokens = latitude.split("°");
+        String newLat = latTokens[0] + "." + latTokens[1];
+        latTokens = newLat.split("'");
+        newLat = latTokens[0] + latTokens[1];
+        latTokens = newLat.split("\"");
+        orentation = latTokens[1];
+        newLat = latTokens[0];//this adds the lat without the last decmial
+        latTokens = newLat.split(".");
+        newLat = latTokens[0];
+        if(orentation == "S"){
+            newLat = "-" + newLat;
+        }
+        location.setLatitude(Double.parseDouble(newLat));
+
+        //longitude parsing
+        String[] longTokens = longtitude.split("°");
+        String newLong = longTokens[0] + "." + longTokens[1];
+        longTokens = newLong.split("'");
+        newLong = longTokens[0] + longTokens[1];
+        longTokens = newLong.split("\"");
+        orentation = longTokens[1];
+        newLong = longTokens[0];//this adds the lat without the last decmial
+        longTokens = newLong.split(".");
+        newLong = longTokens[0];
+        if(orentation == "W"){
+            newLong = "-" + newLong;
+        }
+        location.setLongitude(Double.parseDouble(newLong));
+    }
     public double getLatitude(){
         return this.latitude;
     }
--- app/src/main/java/com/example/mikey/maps/TrailsList.java
+++ app/src/main/java/com/example/mikey/maps/TrailsList.java
@@ -1,10 +1,45 @@
 package com.example.mikey.maps;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  * Created by Workstation2.0 on 11/9/2016.
  */
 
 public class TrailsList {
+    String fileName = "words.txt";
+    String line = null;
+    String[] fields;
+    ArrayList<Trail> trails = new ArrayList<Trail>();
 
+    private TrailsList() {
+        try {
+            // FileReader reads text files in the default encoding.
+            FileReader fileReader = new FileReader(fileName);
 
+            // Always wrap FileReader in BufferedReader.
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            while ((line = bufferedReader.readLine()) != null) {
+            fields = line.split(":");
+            trails.add(new Trail(fields[0],fields[1],fields[2],fields[3]));
+            }
+            // Always close files.
+            bufferedReader.close();
+        } catch (FileNotFoundException ex) {
+            System.out.println(
+                    "Unable to open file '" +
+                            fileName + "'");
+        } catch (IOException ex) {
+            System.out.println(
+                    "Error reading file '"
+                            + fileName + "'");
+            // Or we could just do this:
+            // ex.printStackTrace();
+        }
+    }
 }
