07ae22c6ed3e3fef53a784480c356edee01bd4f0
Michael Nielsen <mnielsen@oswego.edu>
2016-11-18 15:50:53 -0500

    Added Weather Template for Ben

--- app/src/main/java/com/example/mikey/maps/optionsList.java
+++ app/src/main/java/com/example/mikey/maps/optionsList.java
@@ -1,7 +1,9 @@
 package com.example.mikey.maps;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class optionsList extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class optionsList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options_list);
+    }
+
+    public void onClick(View view){
+        Intent intent = new Intent(this, weatherUI.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/mikey/maps/weatherUI.java
@@ -0,0 +1,13 @@
+package com.example.mikey.maps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class weatherUI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_ui);
+    }
+}
