f6c20939bfbed3a2339d4de4e7f27927bf48305d
Apebble38 <clawrenc@oswego.edu>
2016-12-03 21:17:52 -0500

    TrailsActivity working
    Got the trails activity and filter dropdown box working, need to get the
    filters working, fix upp the trail list display and create a trail
    activity

---
+++ app/src/main/java/com/example/mikey/maps/trailsActivity.java
@@ -0,0 +1,44 @@
+package com.example.mikey.maps;
+
+import android.app.ListActivity;
+import android.content.Context;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.mikey.maps.Trails.Trail;
+import com.example.mikey.maps.Trails.TrailsList;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class trailsActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trails);
+        TrailsList trails = new TrailsList(this);
+        ArrayList<Trail> trailList = (ArrayList<Trail>)trails.getTrailList();
+        System.out.println("Number of trails: " + trailList.size());
+        ArrayList<String> trailName = new ArrayList<String>();
+        for(Trail x: trailList){
+            trailName.add(x.getName());
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getListView().getContext(), android.R.layout.simple_list_item_1,trailName);
+        getListView().setAdapter(adapter);
+    }
+
+
+
+
+}
